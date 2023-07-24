0082452f5d36409f629ebba396faa1c14503bcb0
itroyen <inso4ka@mail.ru>
2023-07-24 19:02:57 +0300

    timed out connection added

--- client/client.cpp
+++ client/client.cpp
@@ -10,32 +10,67 @@ void Client::setup()
 
 void Client::getRequestIP()
 {
-    struct addrinfo hints, *res;
-
-    socket_t sockfd = 0;
-    int      n;
+    struct addrinfo   hints, *res;
+    socket_t          sockfd = 0;
+    int               n;
     std::vector<char> buffer(2048);
-
 
     // Get data about IP-address
     memset(&hints, 0, sizeof(hints));
     hints.ai_family   = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    //  if ((n = getaddrinfo(ip.c_str(), "http", &hints, &res)) != 0)
+    // Set timeout for connection
+    struct timeval timeout;
+    timeout.tv_sec  = 10; // Timeout set to 10 seconds
+    timeout.tv_usec = 0;
+
     if ((n = getaddrinfo(m_uri.host.c_str(), m_uri.port.c_str(), &hints, &res)) != 0) {
         LOG_ERROR("getaddrinfo: {}", gai_strerror(n));
         return;
     }
 
-    // Create socket and setup connection
     if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
         LOG_ERROR("Failed to create a socket");
         return;
     }
 
-    if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
-        LOG_ERROR("Failed to connect to the socket");
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout)) < 0) {
+        LOG_ERROR("Failed to set socket receive timeout");
+        CLOSE_SOCKET(sockfd);
         return;
+    }
+
+    if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+        if (errno == EINPROGRESS) {
+            fd_set fds;
+            FD_ZERO(&fds);
+            FD_SET(sockfd, &fds);
+            int status = 0;
+            if ((status = select(sockfd + 1, nullptr, &fds, nullptr, &timeout)) == 1) {
+                int       error = 0;
+                socklen_t len   = sizeof(error);
+                getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len);
+                if (error == 0) {
+                    LOG_INFO("Connected to the server");
+                } else {
+                    LOG_ERROR("Failed to connect to the server: {}", strerror(error));
+                    CLOSE_SOCKET(sockfd);
+                    return;
+                }
+            } else if (status == 0) {
+                LOG_ERROR("Connection timed out");
+                CLOSE_SOCKET(sockfd);
+                return;
+            } else {
+                LOG_ERROR("Failed to select the socket");
+                CLOSE_SOCKET(sockfd);
+                return;
+            }
+        } else {
+            LOG_ERROR("Failed to connect to the socket: {}", strerror(errno));
+            CLOSE_SOCKET(sockfd);
+            return;
+        }
     }
 
     freeaddrinfo(res);
@@ -81,7 +116,7 @@ bool Client::readData(socket_t sockfd, char* buffer, size_t buffer_size) const
     while ((bytes = recv(sockfd, buffer, buffer_size, 0)) > 0) {
         if (!headerEnded) {
             header.append(buffer, bytes);
-            size_t      pos = header.find("\r\n\r\n");
+            size_t pos = header.find("\r\n\r\n");
             if (pos != std::string::npos) {
                 headerEnded = true;
                 std::string contentLengthStr("Content-Length:");
